f5ae3c1e733c0531f3974825c4342831b080507e
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-02-01 23:50:46 +0530

    feat: added auth view model

--- app/src/main/java/com/axionlabs/chimespace/screens/AuthenticationScreen.kt
+++ app/src/main/java/com/axionlabs/chimespace/screens/AuthenticationScreen.kt
@@ -4,19 +4,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.axionlabs.chimespace.viewmodel.AuthViewModel
 
 @Composable
-fun AuthenticationScreen(navController: NavController, modifier : Modifier = Modifier){
-
+fun AuthenticationScreen(navController: NavController, modifier : Modifier = Modifier, authViewModel: AuthViewModel = hiltViewModel()){
+    val isAuthenticated = authViewModel.isAuthenticated.collectAsState().value
 
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
+        if(isAuthenticated){
+            navController.navigate("home")
+        }
         Text("Authentication Screen")
     }
 }
